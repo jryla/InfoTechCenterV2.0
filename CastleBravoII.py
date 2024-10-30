e1a5783ecceba901536b19d86cae412ad3e1ba5b
Ryland <02justin.ryland@gulllakecs.org>
2024-10-30 08:22:37 -0400

    Added a While Loop for Booting our OS

--- CastleBravoII.py
+++ CastleBravoII.py
@@ -1,1 +1,18 @@
+import sys
+import time
+
 print("\nWelcome to InfoTechCenter V2.0")
+
+x = 0
+ellipsis = 0
+
+while x != 20:
+    x+=1
+    message=("InfoTechCenter System Booting" + "." * ellipsis)
+    ellipsis+=1
+    sys.stdout.write("\r"+message)
+    time.sleep(.5)
+    if ellipsis == 4:
+        ellipsis=0
+    if x == 20:
+        print("\n\nOperating System Booted up - Retina Scanned - Access Granted")
