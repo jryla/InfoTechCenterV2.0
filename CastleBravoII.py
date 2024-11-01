a2ec76f3ae0e0b3ac30081bdc86c5ebbeafb0353
Ryland <02justin.ryland@gulllakecs.org>
2024-11-01 07:54:38 -0400

    Added color to Print Function

--- CastleBravoII.py
+++ CastleBravoII.py
@@ -1,6 +1,11 @@
 import sys  # Import the sys module for system-specific parameters and functions
 import time  # Import the time module for time-related functions
 
+green = "\033[92m"
+bold = "\033[1m"
+reset = "\033[0m"
+
+
 # Print a welcome message to the user
 print("\nWelcome to InfoTechCenter V2.0")
 
@@ -11,7 +16,7 @@ ellipsis = 0  # Initialize a variable to track the number of dots for the loadin
 while x != 20:
     x += 1  # Increment the counter
     # Create a loading message that includes an increasing number of dots
-    message = ("InfoTechCenter System Booting" + "." * ellipsis)
+    message = (bold+"InfoTechCenter System Booting" + "." * ellipsis + reset)
     ellipsis += 1  # Increment the ellipsis counter to add more dots
     # Write the message to the console, using '\r' to overwrite the line
     sys.stdout.write("\r" + message)
@@ -21,4 +26,4 @@ while x != 20:
         ellipsis = 0
     # Once the counter reaches 20, print the final boot-up message
     if x == 20:
-        print("\n\nOperating System Booted up - Retina Scanned - Access Granted")
+        print(green + "\n\nOperating System Booted up - Retina Scanned - Access Granted" + reset)
