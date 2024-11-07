ccfff4239ca0ea171d511d4480f8acbf59e9a02a
Ryland <02justin.ryland@gulllakecs.org>
2024-11-07 07:33:22 -0500

    ChatGPT Made my Code more Efficient

--- CastleBravoII.py
+++ CastleBravoII.py
@@ -1,69 +1,44 @@
 # Print decorative header
 print("\n****************************************************\n")
-print("Welcome Branch")
+print("Weather Branch")
 
 # Import required libraries
 import random  # for random selection of weather conditions
 from time import sleep  # for simulating delay in response
 
-
 # Function to randomly select a weather condition
 def weather():
     # List of possible weather conditions
-    weatherForecast = ["snowy", "blizzard", "rainy", "windy", "icy", "sunny"]
+    weather_forecast = ["snowy", "blizzard", "rainy", "windy", "icy", "sunny"]
     # Randomly choose one condition from the list
-    weatherCondition = random.choice(weatherForecast)
-    return weatherCondition
-
+    return random.choice(weather_forecast)
 
 # Generate a random weather alert by calling the weather function
-weatherAlert = weather()
-
+weather_alert = weather()
 
 # Function to simulate a Vehicle Response System (VRS) based on weather
-def vehicleResponseSystem():
-    # Check for "snowy" condition and set response
-    if weatherAlert == "snowy":
-        print("\n The National Weather Service has updated alarm by 35 minutes because"
-              " of the forecast of", weatherAlert, "weather conditions.")
-        sleep(1)
-        print("\n VRS System has been engaged, only allowing 60 mph")
+def vehicle_response_system():
+    # Dictionary to store weather conditions and corresponding responses
+    weather_responses = {
+        "snowy": (35, 60),
+        "blizzard": (45, 55),
+        "rainy": (15, 70),
+        "windy": (15, 70),
+        "icy": (50, 85)
+    }
 
-    # Check for "blizzard" condition and set response
-    elif weatherAlert == "blizzard":
-        print("\n The National Weather Service has updated alarm by 45 minutes because"
-              " of the forecast of", weatherAlert, "weather conditions.")
+    # Check if the weather condition has a specific response
+    if weather_alert in weather_responses:
+        delay, speed_limit = weather_responses[weather_alert]
+        print(f"\n The National Weather Service has updated alarm by {delay} minutes because"
+              f" of the forecast of {weather_alert} weather conditions.")
         sleep(1)
-        print("\n VRS System has been engaged, only allowing 55 mph.")
-
-    # Check for "rainy" condition and set response
-    elif weatherAlert == "rainy":
-        print("\n The National Weather Service has updated alarm by 15 minutes because"
-              " of the forecast of", weatherAlert, "weather conditions.")
-        sleep(1)
-        print("\n VRS System has been engaged, only allowing 70 mph.")
-
-    # Check for "windy" condition and set response
-    elif weatherAlert == "windy":
-        print("\n The National Weather Service has updated alarm by 15 minutes because"
-              " of the forecast of", weatherAlert, "weather conditions.")
-        sleep(1)
-        print("\n VRS System has been engaged, only allowing 70 mph.")
-
-    # Check for "icy" condition and set response
-    elif weatherAlert == "icy":
-        print("\n The National Weather Service has updated alarm by 50 minutes because"
-              " of the forecast of", weatherAlert, "weather conditions.")
-        sleep(1)
-        print("\n VRS System has been engaged, only allowing 85 mph.")
-
-    # If no specific weather condition matches, use this default response
+        print(f"\n VRS System has been engaged, only allowing {speed_limit} mph.")
     else:
-        print("\n The National Weather Service has called for", weatherAlert, "skies,"
-                                                                              " drive carefully.")
+        # Default response for unlisted conditions
+        print(f"\n The National Weather Service has called for {weather_alert} skies, drive carefully.")
         sleep(1)
         print("\n VRS System disengaged")
 
-
 # Call the vehicle response function to execute based on weather condition
-vehicleResponseSystem()
+vehicle_response_system()
